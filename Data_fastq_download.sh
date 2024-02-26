a4d90ecd8828e001e805d49b6672e8b2f25c0831
Wdiniz <wjarles09@gmail.com>
2024-02-25 18:41:50 -0600

    download
    data

---
+++ Data_fastq_download.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/095/SRR14760795/SRR14760795_1.fastq.gz -o SRR14760795_GSM5363738_RNA-seq_Tet1_Mut_Replicate_1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/095/SRR14760795/SRR14760795_2.fastq.gz -o SRR14760795_GSM5363738_RNA-seq_Tet1_Mut_Replicate_1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/099/SRR14760799/SRR14760799_1.fastq.gz -o SRR14760799_GSM5363742_RNA-seq_Tet1_KO_Replicate_2_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/099/SRR14760799/SRR14760799_2.fastq.gz -o SRR14760799_GSM5363742_RNA-seq_Tet1_KO_Replicate_2_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/092/SRR14760792/SRR14760792_1.fastq.gz -o SRR14760792_GSM5363735_RNA-seq_Tet1_WT_Replicate_1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/092/SRR14760792/SRR14760792_2.fastq.gz -o SRR14760792_GSM5363735_RNA-seq_Tet1_WT_Replicate_1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/093/SRR14760793/SRR14760793_1.fastq.gz -o SRR14760793_GSM5363736_RNA-seq_Tet1_WT_Replicate_2_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/093/SRR14760793/SRR14760793_2.fastq.gz -o SRR14760793_GSM5363736_RNA-seq_Tet1_WT_Replicate_2_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/098/SRR14760798/SRR14760798_1.fastq.gz -o SRR14760798_GSM5363741_RNA-seq_Tet1_KO_Replicate_1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/098/SRR14760798/SRR14760798_2.fastq.gz -o SRR14760798_GSM5363741_RNA-seq_Tet1_KO_Replicate_1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/096/SRR14760796/SRR14760796_1.fastq.gz -o SRR14760796_GSM5363739_RNA-seq_Tet1_Mut_Replicate_2_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/096/SRR14760796/SRR14760796_2.fastq.gz -o SRR14760796_GSM5363739_RNA-seq_Tet1_Mut_Replicate_2_Mus_musculus_RNA-Seq_2.fastq.gz
